9fd8dbdcd1b39d03ea63fa3935b591ce315689ac
ohjaoh <ohja1004@naver.com>
2024-10-25 17:50:49 +0900

    [오자현] 초기설정 및  메모

---
+++ SpringFinalProject/src/test/java/Job/SpringFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
