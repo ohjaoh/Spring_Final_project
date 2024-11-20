3e80371f49fcc99cc0a484882260476294eb018e
ohjaoh <ohja1004@naver.com>
2024-11-21 08:26:25 +0900

    [오자현] board엔티티 수정

--- SpringFinalProject/src/main/java/Job/entity/Board.java
+++ SpringFinalProject/src/main/java/Job/entity/Board.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,8 +29,9 @@ public class Board {
 	@NotBlank(message = "제목이 비었습니다.")
 	@Size(max = 100, message = "제목은 100자 이내여야 합니다.") // 최대 길이 제한
 	private String boardTitle; // 게시판 제목
+	
 	@NotBlank(message = "내용이 비었습니다.")
-	@Size(max = 1000, message = "내용은 1000자 이내여야 합니다.") // 최대 길이 제한
+	@Column(columnDefinition = "TEXT")
 	private String boardContent; // 게시판 내용
 
 	@Temporal(TemporalType.TIMESTAMP) // 날짜-시간 형식으로 저장
@@ -43,59 +45,9 @@ public class Board {
 	@Size(max = 20, message = "작성자 타입은 20자 이내여야 합니다.") // 최대 길이 제한
 	private String writerType; // 작성자 타입
 
-	public long getBoardNo() {
-		return boardNo;
-	}
-
-	public void setBoardNo(long boardNo) {
-		this.boardNo = boardNo;
-	}
-
-	public String getBoardTitle() {
-		return boardTitle;
-	}
-
-	public void setBoardTitle(String boardTitle) {
-		this.boardTitle = boardTitle;
-	}
-
-	public String getBoardContent() {
-		return boardContent;
-	}
-
-	public void setBoardContent(String boardContent) {
-		this.boardContent = boardContent;
-	}
-
-	public Date getWriteDate() {
-		return writeDate;
-	}
-
-	public void setWriteDate(Date writeDate) {
-		this.writeDate = writeDate;
-	}
-
-	public String getWriterName() {
-		return writerName;
-	}
-
-	public void setWriterName(String writerName) {
-		this.writerName = writerName;
-	}
-
-	public String getWriterType() {
-		return writerType;
-	}
-
-	public void setWriterType(String writerType) {
-		this.writerType = writerType;
-	}
-
-	@Override
-	public String toString() {
-		return "Board [boardNo=" + boardNo + ", boardTitle=" + boardTitle + ", boardContent=" + boardContent
-				+ ", writeDate=" + writeDate + ", writerName=" + writerName + ", writerType=" + writerType + "]";
-	}
+	@Size(max = 50, message = "게시판 태그는 50자 이내여야 합니다.") // 최대 길이 제한
+	@Column(nullable = false, columnDefinition = "varchar(50) default 'etc'")
+	private String boardTag; // 게시판 태그
 
 	
 }
