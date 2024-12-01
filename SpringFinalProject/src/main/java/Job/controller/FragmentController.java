05dfe9a820276463e5c3ca7076c7b227bb2e9ffc
ohjaoh <ohja1004@naver.com>
2024-12-01 19:44:57 +0900

    코드 정리 - 자현

--- SpringFinalProject/src/main/java/Job/controller/FragmentController.java
+++
@@ -1,8 +0,0 @@
-package Job.controller;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class FragmentController {
-
-}
--- SpringFinalProject/src/test/java/Job/Board/BoardTest.java
+++ SpringFinalProject/src/test/java/Job/Board/BoardTest.java
@@ -23,7 +23,7 @@ public class BoardTest {
 	public void insertBoardTest() {
 		LoginInfo userInfo = new LoginInfo();
 		userInfo.setLoginId("관리자");
-		String boardCategory = "자유";
+		String boardCategory = "구직";
 
 		for (int i = 0; i < 5; i++) {
 			Board board = new Board();
